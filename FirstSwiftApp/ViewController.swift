520c1832b7afb2a253747ea781dbbfe83e276169
jasontoth <jtoth313@gmail.com>
2018-08-16 12:54:39 -0500

    initial push

--- FirstSwiftApp/ViewController.swift
+++ FirstSwiftApp/ViewController.swift
@@ -9,10 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+        
+    @IBOutlet weak var welcomeLabel: UILabel!
+    
+    @IBAction func buttonTap(_ sender: Any) {
+        
+        if welcomeLabel.text == "Jason's App" {
+            welcomeLabel.text = "Button Press"
+        }
+        else if welcomeLabel.text == "Button Press" {
+            welcomeLabel.text = "Button Press Again"
+        }
+        else if welcomeLabel.text == "Button Press Again" {
+            welcomeLabel.text = "Stop"
+        }
+    }
+    @IBAction func changesTap(_ sender: Any) {
+        
+        if self.view.backgroundColor == UIColor.gray {
+            self.view.backgroundColor = UIColor.cyan
+        }
+        else if self.view.backgroundColor == UIColor.cyan {
+            self.view.backgroundColor = UIColor.gray
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        welcomeLabel.text = "Jason's App"
+        //welcomeLabel.textColor.
+        self.view.backgroundColor = UIColor.gray
     }
 
     override func didReceiveMemoryWarning() {
